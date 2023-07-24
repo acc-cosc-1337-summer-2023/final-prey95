1e51e4b3d5602a37fd427cc5aed0ea75f59b0525
prey95 <135293575+prey95@users.noreply.github.com>
2023-07-24 04:48:29 +0000

    Question 1

--- src/die.h
+++ src/die.h
@@ -3,12 +3,17 @@
 
 using std::cout;
 
-class Die {
+#ifndef DIE_H
+#define DIE_H
+
+class Die{
 public:
+    Die(int num){roll_value = num;}
     void roll();
     int rolled_value() const{return roll_value;}
 private:
     int roll_value;
     int sides{6};
-
 };
+
+#endif
