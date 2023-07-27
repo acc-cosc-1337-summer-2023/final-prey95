f823f41885572bdf6e9ef2cb596c7e7b2c8faa75
prey95 <135293575+prey95@users.noreply.github.com>
2023-07-27 19:34:40 +0000

    Question 5

--- src/come_out_phase.cpp
+++ src/come_out_phase.cpp
@@ -1,9 +1,15 @@
 #include "come_out_phase.h"
 
 RollOutcome ComeOutPhase::get_outcome(Roll* roll){
-    if(roll->roll_value() == 7 || roll->roll_value() == 11){
+    if(roll->roll_value() == 7 ){
         return RollOutcome::natural;
-    }else if(roll->roll_value() == 2 || roll->roll_value() == 3 || roll->roll_value() == 12){
+    }else if(roll->roll_value() == 11){
+        return RollOutcome::natural;
+    }else if(roll->roll_value() == 2){
+        return RollOutcome::craps;
+    }else if(roll->roll_value() == 3){
+        return RollOutcome::craps;
+    }else if(roll->roll_value() == 12){
         return RollOutcome::craps;
     }else{
         return RollOutcome::point;
--- src/main.cpp
+++ src/main.cpp
@@ -7,17 +7,33 @@
 
 int main() 
 {
-	Shooter shoot;
+	srand(time(0));
+	Shooter shooter;
+	Roll* roll;
 	Die die1(1), die2(2);
-	shoot.throw_die(die1, die2);
+	roll = shooter.throw_die(die1, die2);
 
-	cout << shoot;
+	ComeOutPhase come_out_phase;
 
-	Shooter shoot1;
-	shoot1.throw_die(die1, die2);
+	while(come_out_phase.get_outcome(roll) == RollOutcome::natural || come_out_phase.get_outcome(roll) == RollOutcome::craps){
+		cout << "Rolled " << roll->roll_value() << " roll again" << "\n";
+		roll = shooter.throw_die(die1, die2); 
+	}
 
-	cout << shoot1;
-	cout << "--------------------" << "\n";
+	cout << "Rolled " << roll->roll_value() << " start of point phase" << "\n";
+	cout << "Roll until " << roll->roll_value() << " or 7 is rolled." << "\n";
+
+	int point = roll->roll_value();
+	roll = shooter.throw_die(die1, die2);
+	PointPhase point_phase(point);
+
+	while(point_phase.get_outcome(roll) != RollOutcome::point && point_phase.get_outcome(roll) != RollOutcome::seven_out){
+		cout << "Rolled " << roll->roll_value() << " roll again" << "\n";
+		roll = shooter.throw_die(die1, die2);
+	}
+
+	cout << "Rolled value: " << roll->roll_value() << "\n" << " End of Point Phase" << "\n";
+	cout << shooter;
 
 
 	return 0;
